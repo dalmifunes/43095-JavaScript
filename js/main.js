49a54c0d73a0a0a1fa1d9175dea8333910650a1e
dalmiro funes <dalmifunes04@gmail.com>
2023-05-10 00:23:42 -0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,43 @@
+alert("hola a todos");
+
+/*let nombre = "Dalmiro"; 
+let apellido = "FUnes";
+var edad= "19";
+const anioNacimiento = 2004
+
+console.log(nombre);
+console.log(apellido);
+console.log(anioNacimiento);
+
+
+let numeroA = 10;
+let numeroB = 20;
+let numeroC = 30;
+
+
+var resultado = numeroA + numeroB
+console.log(resultado);
+
+
+/*let nombreingresado = prompt("ingrese su nombre:");
+let apellido = prompt("ingrese su apellido":);
+alert("nombre: " + nombreingresado + "  " + apellido);*/
+
+
+console.log("inicio");
+
+let i;
+
+//for (let  i= 0;  i<=10;  i=++) {
+    //alert("hola a todos! #" + i;);
+    
+//}
+
+/*let valoringresado = parseInt(prompt("ingrese su numero;"));
+
+for (let i=1;  i<=10; i++) {
+    let resultado = valoringresado * i;
+    alert(valoringresado + " x " + 1 + " = " + resultado);
+}*/
+
+
